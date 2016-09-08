6ab0e9c1a531f50d601805b3b43335050ea4c934
Alex Kulkoff <alex.kulkoff@gmail.com>
2016-09-08 17:29:25 +0500

    added header-adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp'), // Подключаем Gulp
     sass = require('gulp-sass'); //Подключаем Sass пакет
+    browserSync = require('browser-sync'); //Подключаем Обновление браузера
 
 gulp.task('sass', function(){ // Создаем таск "sass"
     return gulp.src('app/sass/style.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/sass/**/*.scss', ['sass']); 
+  // другие ресурсы
 });
