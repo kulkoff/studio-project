0bff3e0f285ac8a39fa7f5e6ad69afe9b84167ce
Alex Kulkoff <alex.kulkoff@gmail.com>
2016-09-16 18:13:58 +0500

    work work

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
     
-    //раскрытие описания у тепловина
+    //Верхнее меню - мобильное
     $(".btn__menu").click(function() {
 	   $(this).toggleClass("btn__menu--active");
      $(".main-nav").toggleClass("main-nav--mobile-opened");
-     $(".main-nav").fadeIn(400);
     });
-  });
+    //Карусель
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:false,
+        center:true,
+        nav:true,
+        margin:10,
+        lazyLoad: true,
+        URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: 'URLHash'
+    });
+ });
